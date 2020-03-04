4b178a87f32246140645541d17d2a5b2f7a85912
Sam Greider <sam.greider7@gmail.com>
2020-03-04 13:33:40 -0500

    Created API structure and controller

---
+++ Migrations/20200304183009_AddedHockeySticksTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ShopAPI.Migrations
+{
+    public partial class AddedHockeySticksTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HockeySticks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SKU = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    NumberInStock = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    DateOrdered = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HockeySticks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HockeySticks");
+        }
+    }
+}
---
+++ Models/HockeyStick.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ShopAPI.Models
+{
+  public class HockeyStick
+  {
+    public int Id { get; set; }
+
+    public int SKU { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public int NumberInStock { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateTime DateOrdered { get; set; }
+  }
+}
